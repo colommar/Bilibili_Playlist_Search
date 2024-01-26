0ba029268f8d0df922c3d0a578659ba6428de4dc
colommar <yfxx_weiyx@163.com>
2024-01-26 14:42:08 +0800

    Update bilibili_time.js
    handle the time length problem

--- bilibili_time.js
+++ bilibili_time.js
@@ -2,14 +2,26 @@ var time_elements = document.querySelectorAll('.length');
 
 var ans = 0;
 
+var message = "There is things wrong!!\nplease vist https://github.com/colommar/Bilibili_Playlist_Search!!\n";
+
 time_elements.forEach(function(time) {
     var time_string = time.innerText;
     var time_array = time_string.split(':');
-    var minutes = parseInt(time_array[0], 10);
-    var seconds = parseInt(time_array[1], 10);
-    ans += minutes * 60 + seconds;
+    if(time_array.length == 3) {
+        var hours = parseInt(time_array[0],10);
+        var minutes = parseInt(time_array[1], 10);
+        var seconds = parseInt(time_array[2], 10);
+        ans += hours * 60 * 60 + minutes * 60 + seconds;
+    }
+    else if(time_array.length == 2) {
+        var minutes = parseInt(time_array[0], 10);
+        var seconds = parseInt(time_array[1], 10);
+        ans += minutes * 60 + seconds;
+    }
+    else {
+        console.log(message);
+    }
 });
-
 
 var hours = Math.floor(ans / 3600);
 var minutes = Math.floor((ans % 3600) / 60);
